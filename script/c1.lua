be05e2e937df562b604a3a03d1442a208fe47029
jonathanluepfert <57803550+jonathanluepfert@users.noreply.github.com>
2021-01-22 21:53:22 +0100

    Create c1.lua

---
+++ script/c1.lua
@@ -0,0 +1,47 @@
+-- Honest (AI)
+local s, id = GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+  --atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCondition(s.condition1)
+	e1:SetCost(s.cost1)
+	e1:SetOperation(s.operation1)
+	c:RegisterEffect(e1)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	local phase=Duel.GetCurrentPhase()
+	if phase~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return d~=nil and d:IsFaceup() and ((a:GetControler()==tp and a:IsAttribute(ATTRIBUTE_LIGHT) and a:IsRelateToBattle())
+		or (d:GetControler()==tp and d:IsAttribute(ATTRIBUTE_LIGHT) and d:IsRelateToBattle()))
+end
+function s.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if not a:IsRelateToBattle() or not d:IsRelateToBattle() then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetOwnerPlayer(tp)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	if a:GetControler()==tp then
+		e1:SetValue(d:GetAttack())
+		a:RegisterEffect(e1)
+	else
+		e1:SetValue(a:GetAttack())
+		d:RegisterEffect(e1)
+	end
+end
